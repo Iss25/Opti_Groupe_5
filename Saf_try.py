772cf677d23dd4d7b5183054c54be592e8da81c7
Safiyand <129692949+Safiyand@users.noreply.github.com>
2023-04-22 12:46:08 +0200

    Update Saf_try.py
    Update temporelle

--- Saf_try.py
+++ Saf_try.py
@@ -5,11 +5,19 @@ from scipy.optimize import linprog
 import math
 import time
 
-dt = 15*60 
+'''
+Tache 1:
+On souhaite que la température du bâtiment reste
+comprise dans une certaine plage admissible de températures,
+et on cherche à minimiser le coût total de l'électricité consommée par la pompe à chaleur.
+'''
+ 
+
+dt = 15*60 #en seconde
 nb_intervalles = 672 #nombres d'intervalles de 15 minutes sur 7 jours
 temperatures_ext = np.load("Temperatures-Montreal.npy") # donées des températures à Montreal
 eta = 0.99 # coefficient relatif à l'isolation
-capacite_calorifique = 10/4 * 360 #kWh pour chauffer de 1°c
+capacite_calorifique = 10/4  #kWh pour chauffer de 1°c
 
 T_min = 19 #Température minimale du batiment
 T_max = 21 #Température maximale du batiment
@@ -18,7 +26,7 @@ cout_elec = np.full(nb_intervalles,0.26) #initalisation du cout de l'élec a 0.2
 for i in range(7):
     for j in range(96):
         if j < 28 or j>= 88: #réajustement du cout de l'élec a 0.18 entre 22h et 7h
-            cout_elec[j+i*96] = 0.18 
+            cout_elec[j+i*96] = 0.18
             
 COPT_reverse = 3.2 #COPT de la pompe quand on refroidi
 def COP_normal(T_ext):
@@ -43,8 +51,8 @@ contraintes_1 += [T_int[i] <= T_max for i in range(nb_intervalles)]
 for i in range(nb_intervalles - 1):
 
     contraintes_1 += [T_int[i + 1] - T_int[i] == - (1 - eta) * ( T_int[i]- temperatures_ext[i]) + #perte de temp sans action
-                        (COP_normal(temperatures_ext[i]) * P_chauff[i] * dt / capacite_calorifique)  - #augmentation de la temp en mode normal
-                        (COPT_reverse * P_refroid[i] * dt / capacite_calorifique)] #diminution de la temp en mode reverse
+                        (COP_normal(temperatures_ext[i]) * P_chauff[i] * dt/(60*capacite_calorifique)) - #augmentation de la temp en mode normal
+                        (COPT_reverse * P_refroid[i] * dt/(60*capacite_calorifique))] #diminution de la temp en mode reverse
     
 
 #Contrainte sur la positivité des puissances et max KW
@@ -54,7 +62,7 @@ contraintes_1 += [P_chauff <= 1]
 contraintes_1 += [P_refroid <= 1]
 
 ## Initialisation du cout total :
-cost = cp.sum(cout_elec * (P_chauff + P_refroid)) 
+cost = cp.sum(cout_elec * (P_chauff + P_refroid)*4) 
 
 start_time1 = time.time()
 
@@ -89,9 +97,13 @@ axs[1][0].set_xlabel("Intervalle de temps")
 axs[1][0].set_ylabel("Puissance (kW)")
 axs[1][0].legend()
 
-plt.show()
 
 
+'''
+Tache 2:
+0n cherche à minimiser l'inconfort total (somme des inconforts sur toute la période considérée)
+tout en respectant la contrainte de budget.
+'''
 
 
 
