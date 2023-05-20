15ecb6761b7e37d20c4bcc6fc3754428a7629839
Iss25 <114648812+Iss25@users.noreply.github.com>
2023-05-20 18:00:18 +0200

    test pr péna quad

--- iss_code.py
+++ iss_code.py
@@ -102,6 +102,59 @@ def basic(first_interval_idx,x, max_cost=math.inf):
     #print(output)
     return output
 
+def pena(first_interval_idx, max_cost=math.inf):
+    inconfort_mode = max_cost != math.inf
+    last_interval_idx = first_interval_idx + computing_intervals_amount                                 
+    temperatures_ext = temperatures_montreal[first_interval_idx:last_interval_idx]
+
+    p_warming = cp.Variable(computing_intervals_amount, nonneg=True)                                    # Puissance de la pompe à l'intervalle i en réchauffement
+    p_reverse = cp.Variable(computing_intervals_amount, nonneg=True)                                    # Puissance de la pompe à l'intervalle i en reverse
+    temperatures_int = cp.Variable(computing_intervals_amount)                                          # Températures intérieures
+    partial_electricity_cost = electricity_cost[first_interval_idx:last_interval_idx]                   # Coût de l'électricité sur la période sélectionnée
+    
+    cost = cp.sum(partial_electricity_cost @ (p_warming + p_reverse))
+
+    constraints = [p_warming >= 0]
+    constraints += [p_reverse >= 0]
+    constraints += [p_warming <= max_pump_power]
+    constraints += [p_reverse <= max_pump_power]
+    constraints += [temperatures_int[0] == mid_temperature]
+    constraints += [temperatures_int[-1] == mid_temperature]
+    constraints += [temperatures_int[1:] == next_temperature(temperatures_int[:-1], temperatures_ext[:-1]) 
+    + cp.multiply(COP_warming(temperatures_ext[:-1]), p_warming[:-1]) * Cp 
+    - cp.multiply(COP_reverse(), p_reverse[:-1]) * Cp
+    ]
+
+    if inconfort_mode:
+        inconforts_sup = cp.Variable(computing_intervals_amount, nonneg=True)
+        inconforts_inf = cp.Variable(computing_intervals_amount, nonneg=True)
+        constraints += [cost <= max_cost]
+        constraints += [temperatures_int - T_min >= -inconforts_inf]
+        constraints += [temperatures_int - T_max <= inconforts_sup]
+        objective = 0
+        for k in range(computing_intervals_amount-1):
+            objective += (((inconforts_sup[k]+inconforts_sup[k+1])*inconfort_penality_supp)/2) + (((inconforts_inf[k]+inconforts_inf[k+1])*inconfort_penality_inf)/2)
+        #objective = cp.sum(inconforts_sup*inconfort_penality_supp + inconfort_penality_inf*inconforts_inf)
+        #objective = cp.sum(sum(inconforts_sup[i]*inconfort_penality_supp * ((inconforts_sup[i])**2) for i in range(computing_intervals_amount))
+        #         + sum(inconforts_inf[i]*inconfort_penality_inf * ((inconforts_inf[i])**2) for i in range(computing_intervals_amount)))
+    else:
+        constraints += [temperatures_int >= T_min]
+        constraints += [temperatures_int <= T_max]
+        objective = cost 
+
+    problem = cp.Problem(cp.Minimize(objective), constraints)
+    start_time = time.time()
+    solution = problem.solve(solver=cp.SCIPY, scipy_options={"method": "highs"}, warm_start=inconfort_mode)
+    #solution = problem.solve()
+    end_time = time.time()
+
+    if cost.value is None: 
+        print("None")
+        return []
+    output = [temperatures_int.value, p_warming.value, p_reverse.value, cost.value, problem.value, end_time - start_time]
+    #print(output)
+    return output
+
 
 def plot_1(period_1, period_2, period_1_first_interval_idx, period_2_first_interval_idx): 
     periods = [period_1, period_2]
@@ -219,21 +272,20 @@ def plot_3(period_1, period_2, period_1_first_interval_idx, period_2_first_inter
     fig.canvas.manager.set_window_title("Tâche 3 - Minimisation de l'inconfort avec budget restreint à {step}n%".format(step=task_3_step))
     plt.show()
 
-output_1_ref = basic(ref_week_start_idx,4)
-print("Computed output_1_ref in {time}s".format(time=output_1_ref[-1]))
-output_1_arbitrary = basic(arbitrary_week_start_idx,4)
-print("Computed output_1_arbitrary in {time}s".format(time=output_1_arbitrary[-1]))
+# output_1_ref = basic(ref_week_start_idx,4)
+# print("Computed output_1_ref in {time}s".format(time=output_1_ref[-1]))
+# output_1_arbitrary = basic(arbitrary_week_start_idx,4)
+# print("Computed output_1_arbitrary in {time}s".format(time=output_1_arbitrary[-1]))
 
-output_1_ref_2 = basic(ref_week_start_idx,2)
-print("Computed output_1_ref in {time}s".format(time=output_1_ref[-1]))
-output_1_arbitrary_2 = basic(arbitrary_week_start_idx,2)
-print("Computed output_1_arbitrary in {time}s".format(time=output_1_arbitrary[-1]))
+# output_1_ref_2 = basic(ref_week_start_idx,2)
+# print("Computed output_1_ref in {time}s".format(time=output_1_ref[-1]))
+# output_1_arbitrary_2 = basic(arbitrary_week_start_idx,2)
+# print("Computed output_1_arbitrary in {time}s".format(time=output_1_arbitrary[-1]))
+output_2_ref = pena(ref_week_start_idx, 8.096*task_2_budget_coefficient)
+print("Computed output_2_arbitrary in {time}s".format(time=output_2_ref[-1] if len(output_2_ref) > 0 else 'error'))
 
-# output_2_ref = basic(ref_week_start_idx,4, output_1_ref[3]*task_2_budget_coefficient)
-# print("Computed output_2_arbitrary in {time}s".format(time=output_2_ref[-1] if len(output_2_ref) > 0 else 'error'))
-
-# output_2_arbitrary = basic(arbitrary_week_start_idx,4, output_1_arbitrary[3]*task_2_budget_coefficient)
-# print("Computed output_2_arbitrary in {time}s".format(time=output_2_arbitrary[-1] if len(output_2_arbitrary) > 0 else 'error'))
+output_2_arbitrary = pena(arbitrary_week_start_idx, 2.898*task_2_budget_coefficient)
+print("Computed output_2_arbitrary in {time}s".format(time=output_2_arbitrary[-1] if len(output_2_arbitrary) > 0 else 'error'))
 
 # start = time.time()
 # output_3_ref = task3(ref_week_start_idx, output_1_ref[3])
@@ -243,7 +295,7 @@ print("Computed output_1_arbitrary in {time}s".format(time=output_1_arbitrary[-1
 # output_3_arbitrary = task3(arbitrary_week_start_idx, output_1_arbitrary[3])
 # print("Computed output_3_arbitrary in {time}s".format(time=time.time()-start))
 
-plot_1(output_1_ref, output_1_arbitrary, ref_week_start_idx, arbitrary_week_start_idx) 
-plot_1(output_1_ref_2, output_1_arbitrary_2, ref_week_start_idx, arbitrary_week_start_idx) 
-#plot_2(output_2_ref, output_2_arbitrary, ref_week_start_idx, arbitrary_week_start_idx)
+#plot_1(output_1_ref, output_1_arbitrary, ref_week_start_idx, arbitrary_week_start_idx) 
+#plot_1(output_1_ref_2, output_1_arbitrary_2, ref_week_start_idx, arbitrary_week_start_idx) 
+plot_2(output_2_ref, output_2_arbitrary, ref_week_start_idx, arbitrary_week_start_idx)
 #plot_3(output_3_ref, output_3_arbitrary, ref_week_start_idx, arbitrary_week_start_idx)
